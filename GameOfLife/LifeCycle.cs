0eb55e04fe9d58e19abbacfeaf608e3d1b68ff8b
Jakub Piasecki <jakkubpiasecki87@gmail.com>
2021-12-23 14:07:52 +0100

    123

--- GameOfLife/LifeCycle.cs
+++ GameOfLife/LifeCycle.cs
@@ -7,8 +7,6 @@
         {
             int suma;
 
-
-
             for (int i = 1; i < Patterns.tabHeight - 1; i++)
             {
                 for (int j = 1; j < Patterns.tabWidth - 1; j++)
@@ -60,20 +58,3 @@
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
