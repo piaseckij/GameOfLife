bd9c93c7c89e1c75173a3117c8020515864f80d2
piaseckij <jakkubpiasecki87@gmail.com>
2021-12-23 13:54:15 +0100

    code cleanup

--- GameOfLife/LifeCycle.cs
+++ GameOfLife/LifeCycle.cs
@@ -22,26 +22,18 @@
 
                     if (pat.board[i, j] == 1 && (suma == 2 || suma == 3))
                     {
-
                         pat.board2[i, j] = 1;
-
-
                     }
-
 
                     else if (pat.board[i, j] == 0 && suma == 3)
                     {
-
                         pat.board2[i, j] = 1;
-
                     }
+
                     else
                     {
-
                         pat.board2[i, j] = 0;
                     }
-
-
                 }
             }
 
@@ -49,11 +41,10 @@
             pat.board2 = pat.board;
             pat.board = temp;
 
-
-
             Patterns.ClearTable(pat.board2);
 
         }
+
         public static void Live(Patterns pat)
         {
             int z = 0;
@@ -67,12 +58,22 @@
 
             } while (z == 0);
         }
-
-
-
-
-
-
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
